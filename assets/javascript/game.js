7b57df25adabc1cffbfc0463c9112fb37b0efa86
Faolun <james.coatesiii@gmail.com>
2018-06-19 22:38:34 -0400

    final with glow effects and audio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,13 +7,24 @@ var wins= 0;
 var losses= 0;
 var numberOptions = [10, 5, 3, 7, 8];
 var gameRunning = false;
+var patientInt= 0;
+
+var redSound = new Audio("assets/sounds/red.wav");
+var orangeSound = new Audio("assets/sounds/orange.wav");
+var greenSound = new Audio("assets/sounds/green.wav");
+var blueSound = new Audio("assets/sounds/blue.wav");
+var indigoSound = new Audio("assets/sounds/indigo.mp3");
+var failSound = new Audio("assets/sounds/fail.wav");
+var winSound = new Audio("assets/sounds/win.wav")
 
 
 
 function newGame() {
     gameRunning = true;
     counter= 0;
+    patientInt++;
     $(".current-number").text(counter);
+    $("#patientNumb").text("Patient Number " + patientInt +"!")
     
     $("#newGameButton").prop('disabled', true);
     //create new targetNumber
@@ -33,18 +44,64 @@ function newGame() {
     $("#indigo").attr("data-potionsValue", numberOptions[4])
 
 }
+$("#red").on("click", function() {
+  if(redSound.paused) {
+    redSound.play();
+}
+else {
+  redSound.currentTime = 0;
+}
+})
+$("#orange").on("click", function() {
+  if(orangeSound.paused) {
+    orangeSound.play();
+}
+else {
+  orangeSound.currentTime = 0;
+}
+})
+$("#green").on("click", function() {
+  if(greenSound.paused) {
+    greenSound.play();
+}
+else {
+  greenSound.currentTime = 0;
+}
+})
+$("#blue").on("click", function() {
+  if(blueSound.paused) {
+    blueSound.play();
+}
+else {
+  blueSound.currentTime = 0;
+}
+})
+$("#indigo").on("click", function() {
+  if(indigoSound.paused) {
+    indigoSound.play();
+}
+else {
+  indigoSound.currentTime = 0;
+}
+})
+
+
 
 $(".potions").on("click", function() {
+
 
     if (gameRunning === true) {
 
     var potionsValue = ($(this).attr("data-potionsValue"));
+
+    
     
     potionsValue = parseInt(potionsValue);
     counter += potionsValue;
     $(".current-number").text(counter);
     
     if (counter === targetNumber) {
+        winSound.play();
         wins++;
         gameRunning = false;
         $("#wins").text(wins);
@@ -52,14 +109,15 @@ $(".potions").on("click", function() {
       }
   
       else if (counter >= targetNumber) {
+        failSound.play();
         losses++;
         gameRunning = false;
         $("#losses").text(losses);
         $("#newGameButton").prop('disabled', false);
       }
-  
     }
-});
+    });
+
 
 
 function shuffle(array) { //fisher Yates
